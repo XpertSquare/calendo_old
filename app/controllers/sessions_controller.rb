3091e38bbf6d597d272aee29efe45b05ac33b42e
Marius Serban - Office <marius.serban@gmail.com>
2014-03-11 01:22:37 -0700

    Update login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,30 +1,40 @@
 class SessionsController < ApplicationController
+  layout false
   
   def new
+    @login = Login.new
   end
   
   def create
-    user = User.unscoped.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
-      if params[:remember_me]
-        cookies.permanent[:_calendo_auth_token] = {
-                                            :value => user.auth_token,
-                                            :domain => '.lvh.me',
-                                            :expires => 1.month.from_now
-                                            
-                                          }        
+    @login = Login.new(params[:login])
+    if @login.valid? 
+      user = User.unscoped.find_by_email(@login.email)
+      if user && user.authenticate(@login.password)
+        if params[:remember_me]
+          cookies.permanent[:_calendo_auth_token] = {
+                                              :value => user.auth_token,
+                                              :domain => '.lvh.me',
+                                              :expires => 1.month.from_now
+                                              
+                                            }        
+        else
+          cookies[:_calendo_auth_token] = { :value => user.auth_token, 
+                                            :domain => '.lvh.me' }
+        end
+        
+          logger.debug  "You are now logged in! " + user.account.name   + " +++  " + return_url
+          redirect_to admin_url(:subdomain=>user.account.subdomain), :notice => "You are now logged in! " + user.account.name  + " +++  " + return_url  
+          #redirect_to return_url, :notice => "You are now logged in! " + user.account.id.to_s  + " +++  " + return_url
+         
       else
-        cookies[:_calendo_auth_token] = { :value => user.auth_token, 
-                                          :domain => '.lvh.me' }
+        flash.now.alert = "Email or password is invalid"
+        render "new"
       end
-      
-        logger.debug  "You are now logged in! " + user.account.name   + " +++  " + return_url
-        redirect_to admin_url(:subdomain=>user.account.subdomain), :notice => "You are now logged in! " + user.account.name  + " +++  " + return_url  
-        #redirect_to return_url, :notice => "You are now logged in! " + user.account.id.to_s  + " +++  " + return_url 
     else
       flash.now.alert = "Email or password is invalid"
       render "new"
     end
+
   end
   
   def destroy
@@ -34,4 +44,5 @@ class SessionsController < ApplicationController
     redirect_to root_url, notice: "You are now logged out!"
   end
   
+    
 end
