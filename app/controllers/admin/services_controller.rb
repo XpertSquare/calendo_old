6e5d7e7f7d852874cc6588bbbcea89d591083795
Marius Serban - Office <marius.serban@gmail.com>
2014-03-17 23:25:22 -0700

    Create a new service via Ajax calls

--- app/controllers/admin/services_controller.rb
+++ app/controllers/admin/services_controller.rb
@@ -4,5 +4,37 @@ class Admin::ServicesController < ApplicationController
   
   def index
     @services = Service.all
+    @service = Service.new
   end
+  
+   def create
+    @service = Service.new(post_params)   
+    
+    if @service.save 
+      flash[:notice] = "The service was successfully created."   
+      respond_to do |format|
+        format.js
+        format.html { redirect_to admin_services_path }
+        format.json { render action: 'show', status: :created, location: @service }
+      end
+    else
+      flash[:notice] = "There was an error creating the service." 
+      logger.info "Error when saving service"
+      @service.errors.full_messages.each do |msg| 
+        logger.info msg
+      end
+      
+      respond_to do |format|
+        format.js
+        format.html { render action: 'new' }
+        format.json { render json: @service.errors, status: :unprocessable_entity }
+      end
+    end
+
+  end
+  
+  
+  def post_params
+      params.require(:service).permit(:name, :description, :duration, :price)
+    end
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,5 +1,8 @@
 class Service < ActiveRecord::Base
   belongs_to :account
   
+  validates_presence_of :name,:description, :duration, :price
+  validates :price, :format => { :with => /\A\d{1,6}(\.\d{0,2})?\z/ }
+  
   default_scope { where(account_id: Account.current_id) }
 end
