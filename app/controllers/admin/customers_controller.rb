a95d7dd76596b4354189ea76d2a5ba23d77dffb7
Marius Serban - Office <marius.serban@gmail.com>
2014-03-24 22:27:33 -0700

    Add customer_admin layout and use it in customer show action

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,13 @@
 class Admin::CustomersController < ApplicationController
-  layout "account"
+  #layout "account", :only => :index
+  layout "customer_admin", :except => :index
   before_action :authorize!
   before_action :set_customer, only: [:show, :edit, :update]
   
   def index
-    @customers = User.all.with_role(:customer)    
+    @customers = User.all.with_role(:customer) 
+    render(:layout => "layouts/account")
+       
   end
   
   def new
